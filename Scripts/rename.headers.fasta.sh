cc285ed834c414c79d97b57e72c67337295dc50b
Joseph Garza <jgarza189@islander.tamucc.edu>
2025-01-22 18:12:35 -0600

    updated rename headers script

--- Scripts/rename.headers.fasta.sh
+++ Scripts/rename.headers.fasta.sh
@@ -15,13 +15,18 @@ awk '
     # For headers starting with >lcl|Query_, leave them unchanged
     /^>lcl\|Query_/ { print; next }
 
-    # For other headers, extract the organism name and rename the header
+    # For other headers, extract the accession number and organism name, then rename the header
     /^>/ {
+        # Extract accession number from the header (e.g., >gb|AY258488.1|)
+        if (match($0, /gb\|([^\|]+)\|/)) {
+            accession_number = substr($0, RSTART+3, RLENGTH-3)  # Extract accession number
+        }
+
         # Extract organism name from the header using sub()
         if (match($0, /\[organism=([^\]]+)\]/)) {
             organism_name = substr($0, RSTART+10, RLENGTH-11)  # Extract organism name
             gsub(/ /, ".", organism_name)  # Replace spaces in organism name with periods
-            print ">" organism_name  # Print the new header
+            print ">" accession_number "_" organism_name  # Print the new header with accession number and organism name
         } else {
             print $0  # Print the original header if no organism info is found
         }
